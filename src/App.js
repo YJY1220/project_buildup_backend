cf59431b6f07fd7d6acfd284ed50b0a995076183
YJY1220 <jiyeyu1220@gmail.com>
2022-03-18 01:40:34 +0900

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import RestAPI from "./RestAPI.js";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <RestAPI />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
